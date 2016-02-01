4320ca0f762a5bcceb570e0c6408aa5a360d7b02
jeyziel <jeyzielgato@gmail.com>
2016-02-01 18:01:01 -0200

    inicio do layout

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Loja Virtual</title>
+        <link href="css/vendors/plugins.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href='http://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
+        
+    </head>
+    <body>
+        <header class="container">
+            <div class="">
+                <div class="logo-container">
+                    <div class="col-md-10">
+                        <img src="images/logo.png" class="img-responsive" alt="">
+
+                    </div>
+                </div>
+                <div class="col-md-4 col-md-offset-4">
+                    <form action="">
+                        <input class="form-control" type="text" placeholder="pesquisar no site...">
+                        
+                    </form>  
+                </div> 
+            </div>
+        </header>     
+    </body>
+</html>
